832fba31b32ec9498f53867a3c36bfd1ea819553
sa.cux <sa.cux.dev@gmail.com>
2025-03-03 14:56:16 +0100

    feat: add getSpotifyAuthHeader auth utility function

--- src/app/api/auth/utils.ts
+++ src/app/api/auth/utils.ts
@@ -1,3 +1,10 @@
+export function getSpotifyAuthHeader(
+  clientId: string,
+  clientSecret: string,
+): string {
+  return `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString("base64")}`;
+}
+
 // ensures a minimum execution time for smoother transitions and better perceived performance (useful for loaders, spinners, etc.)
 export async function setMinimumDelay(startTime: number, minTime = 1200) {
   const elapsedTime = Date.now() - startTime;
